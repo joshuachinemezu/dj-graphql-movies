b2a7a316f9a9f7d324fd3f36d7d78ecda1bebe96
Joshua Chinemezu <joshuachinemezu@gmail.com>
2020-10-04 13:21:54 +0400

    Add db configurations

--- dj_graphql_movies/settings.py
+++ dj_graphql_movies/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import environ
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,13 +85,8 @@ WSGI_APPLICATION = 'dj_graphql_movies.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(default=env('DATABASE_URL'))
 
 
 # Password validation
